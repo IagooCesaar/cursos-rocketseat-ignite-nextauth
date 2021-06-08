bc39a89c3858e1c6375aaf344ef310759278c0c0
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-08 15:28:35 -0300

    tipagem com generic para validação do retorno da função

--- src/utils/withSSRGuest.ts
+++ src/utils/withSSRGuest.ts
@@ -1,9 +1,9 @@
-import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import { parseCookies } from "nookies";
 
-export function withSSRGuest(fn: GetServerSideProps) {
+export function withSSRGuest<P>(fn: GetServerSideProps<P>) {
   // High Order Function - HOR (função que retorna função)
-  return async (context: GetServerSidePropsContext) => {
+  return async (context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(context);
 
     if (cookies['nextauth.token']) {
