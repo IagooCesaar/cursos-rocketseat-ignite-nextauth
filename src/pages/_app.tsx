343b6ff7952f1108555e04c15ce779a7dd2aaab9
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-08 11:35:54 -0300

    provendo contexto de autenticação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router'
 import '../../styles/globals.css'
+import { AuthProvider } from '../contexts/AuthContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
 export default MyApp
