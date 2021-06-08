3034060f39dab8db658a0ed947a3864cc73a1940
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-08 20:04:41 -0300

    exemplo componente para validaçao de permissão

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,15 +1,12 @@
 import { useEffect } from "react";
+import { Can } from "../components/can";
 import { useAuth } from "../contexts/AuthContext";
-import { useCan } from "../hooks/useCan";
 import { setupApiClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
   const { user } = useAuth();
-  const userCanSeeMetrics = useCan({
-    permissions: ["metrics.list"],
-  })
 
   useEffect(() => {
     api.get('/me')
@@ -20,8 +17,9 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
-
-      { userCanSeeMetrics && <div>Métricas</div>}
+      <Can permissions={["metrics.list"]}>
+        Métricas
+      </Can>
     </>
   )
 }
