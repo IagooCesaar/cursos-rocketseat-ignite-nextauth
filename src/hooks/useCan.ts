9d63cc5ce120afd7ee3b8f7574d3f8c8eb5098c0
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-08 16:53:44 -0300

    melhorias no hook de permissão

--- src/hooks/useCan.ts
+++ src/hooks/useCan.ts
@@ -14,21 +14,24 @@ export function useCan({
   }
 
   if(permissions?.length > 0) {
-    // verifica se usuário possui todas as permissões
+    // verifica se usuário possui todas as permissões necessárias
     const hasAllPermissions = permissions.every(permission => {
       return user.permissions.includes(permission);
     })
+
     if (!hasAllPermissions) {
       return false;
     }
   }
 
+
   if(roles?.length > 0) {
-    // verifica se usuário possui todas as permissões
-    const hasAllRoles = roles.every(role => {
+    // verifica se usuário possui um ou mais perfis necessários
+    const hasSomeRoles = roles.some(role => {
       return user.roles.includes(role);
     })
-    if (!hasAllRoles) {
+
+    if (!hasSomeRoles) {
       return false;
     }
   }
