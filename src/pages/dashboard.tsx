20c8dbef6fd5d3ceb4da9e2bce033fbfe69f9fd7
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-08 13:55:38 -0300

    teste obter dados utilizando token contido no cookie

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext";
+import { api } from "../services/api";
 
 export default function Dashboard() {
+  useEffect(() => {
+    api.get('/me').then((response) => console.log('dashboard', response))
+  }, [])
+
   const { user } = useAuth();
   return (
     <h1>Dashboard: {user?.email}</h1>
